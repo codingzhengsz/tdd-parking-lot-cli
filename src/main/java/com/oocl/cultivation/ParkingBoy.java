a31964da8703e5234baee51865dc20e6317742f5
Martin <codingzhengsz@163.com>
2020-07-24 17:05:28 +0800

    Revert "[ParkingLot][Martin] feature: pass should_return_tickets_when_given_2_car_and_a_parking_boy"
    This reverts commit 8d98d487

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -3,11 +3,7 @@ package com.oocl.cultivation;
 public class ParkingBoy {
 
     public String[] park(Car[] cars) {
-        String[] result = new String[cars.length];
-        for (int index = 0; index < cars.length; index++) {
-            result[index] = cars[index].getLicense();
-        }
-        return result;
+        return new String[]{cars[0].getLicense()};
     }
 
 }
