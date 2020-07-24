8d98d48762f00c3994c2c602c11025e3b3080568
Martin <codingzhengsz@163.com>
2020-07-24 16:43:30 +0800

    [ParkingLot][Martin] test: pass should_return_tickets_when_given_2_car_and_a_parking_boy

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -3,7 +3,11 @@ package com.oocl.cultivation;
 public class ParkingBoy {
 
     public String[] park(Car[] cars) {
-        return new String[]{cars[0].getLicense()};
+        String[] result = new String[cars.length];
+        for (int index = 0; index < cars.length; index++) {
+            result[index] = cars[index].getLicense();
+        }
+        return result;
     }
 
 }
