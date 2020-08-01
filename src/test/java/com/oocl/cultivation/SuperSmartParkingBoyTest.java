b36394cd7ac420fef20eaeb68ebe0c0d1f0c5660
Martin <codingzhengsz@163.com>
2020-08-01 16:46:24 +0800

    [#5][Martin] feature: pass should_return_ticket_when_park_given_a_car_and_a_super_smart_parking_boy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -1,14 +1,14 @@
 package com.oocl.cultivation;
 
 import com.oocl.cultivation.entity.Car;
+import com.oocl.cultivation.entity.Ticket;
 import com.oocl.cultivation.exception.NotEnoughPositionException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SuperSmartParkingBoyTest {
 
@@ -30,7 +30,8 @@ public class SuperSmartParkingBoyTest {
   }
 
   @Test
-  void should_get_parking_lot1_when_parking_given_a_super_smart_parking_boy_and_a_parking_boy() {
+  void
+      should_park_2_cars_into_lot1_and_6_into_lot2_when_parking_given_a_super_smart_parking_boy_and_8_cars_and_2_lots() {
     // given
     ParkingLot parkingLot1 = new ParkingLot("1", 10);
     ParkingLot parkingLot2 = new ParkingLot("2", 50);
@@ -45,5 +46,20 @@ public class SuperSmartParkingBoyTest {
     // then
     assertEquals(8, parkingLot1.getEmptyPositionNumber());
     assertEquals(44, parkingLot2.getEmptyPositionNumber());
+  }
+
+  @Test
+  void should_return_ticket_when_park_given_a_car_and_a_super_smart_parking_boy() {
+    // given
+    ParkingLot parkingLot = new ParkingLot("1", 10);
+    SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
+    superSmartParkingBoy.setParkingLots(Collections.singletonList(parkingLot));
+    Car car = new Car();
+
+    // when
+    Ticket ticket = superSmartParkingBoy.parking(car);
+
+    // then
+    assertNotNull(ticket);
   }
 }
