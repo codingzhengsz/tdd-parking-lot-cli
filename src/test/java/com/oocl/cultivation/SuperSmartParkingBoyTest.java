d08322317934d32adcc41630aa156d3ece4376eb
Martin <codingzhengsz@163.com>
2020-08-01 16:50:57 +0800

    [#5][Martin] test: should_throw_parked_car_exception_when_park_given_a_parked_car_and_a_super_smart_parking_boy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import com.oocl.cultivation.entity.Car;
 import com.oocl.cultivation.entity.Ticket;
 import com.oocl.cultivation.exception.NotEnoughPositionException;
+import com.oocl.cultivation.exception.RepeatedParkingException;
 import com.oocl.cultivation.exception.UnrecognizedPackingTicketException;
 import org.junit.jupiter.api.Test;
 
@@ -121,7 +122,7 @@ public class SuperSmartParkingBoyTest {
 
   @Test
   void
-  should_throw_unrecognized_parking_ticket_exception_when_fetch_given_a_used_ticket_and_a_smart_parking_boy() {
+      should_throw_unrecognized_parking_ticket_exception_when_fetch_given_a_used_ticket_and_a_super_smart_parking_boy() {
     // given
     ParkingLot parkingLot = new ParkingLot("1", 10);
     SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
@@ -132,10 +133,28 @@ public class SuperSmartParkingBoyTest {
     // when
     superSmartParkingBoy.fetching(ticket);
     UnrecognizedPackingTicketException exception =
-            assertThrows(
-                    UnrecognizedPackingTicketException.class, () -> superSmartParkingBoy.fetching(ticket));
+        assertThrows(
+            UnrecognizedPackingTicketException.class, () -> superSmartParkingBoy.fetching(ticket));
 
     // then
     assertEquals("Unrecognized parking ticket.", exception.getMessage());
+  }
+
+  @Test
+  void
+      should_throw_parked_car_exception_when_park_given_a_parked_car_and_a_super_smart_parking_boy() {
+    // given
+    ParkingLot parkingLot = new ParkingLot("1", 10);
+    SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
+    superSmartParkingBoy.setParkingLots(Collections.singletonList(parkingLot));
+    Car parkedCar = new Car();
+    superSmartParkingBoy.parking(parkedCar);
+
+    // when
+    RepeatedParkingException exception =
+        assertThrows(RepeatedParkingException.class, () -> superSmartParkingBoy.parking(parkedCar));
+
+    // then
+    assertEquals("This car is parked", exception.getMessage());
   }
 }
