ac2a25dd8cf763624d66c0219409d47cb762459d
Martin <codingzhengsz@163.com>
2020-08-01 16:46:54 +0800

    [#5][Martin] test: should_return_tickets_when_park_given_2_car_and_a_super_smart_parking_boy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -62,4 +62,23 @@ public class SuperSmartParkingBoyTest {
     // then
     assertNotNull(ticket);
   }
+
+  @Test
+  void should_return_tickets_when_park_given_2_car_and_a_super_smart_parking_boy() {
+    // given
+    ParkingLot parkingLot = new ParkingLot("1", 10);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+    smartParkingBoy.setParkingLots(Collections.singletonList(parkingLot));
+    Car carA = new Car();
+    Car carB = new Car();
+
+    // when
+    Ticket ticketA = smartParkingBoy.parking(carA);
+    Ticket ticketB = smartParkingBoy.parking(carB);
+
+    // then
+    assertNotNull(ticketA);
+    assertNotNull(ticketB);
+    assertNotEquals(ticketA, ticketB);
+  }
 }
