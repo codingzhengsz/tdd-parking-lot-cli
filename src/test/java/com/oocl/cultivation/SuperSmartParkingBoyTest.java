dbfea0e4a1df414712bf771db419d6d2bb778453
Martin <codingzhengsz@163.com>
2020-08-01 16:43:10 +0800

    [#5][Martin] feature: pass should_get_parking_lot1_when_parking_given_a_super_smart_parking_boy_and_a_parking_boy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -4,6 +4,7 @@ import com.oocl.cultivation.entity.Car;
 import com.oocl.cultivation.exception.NotEnoughPositionException;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,7 +14,7 @@ public class SuperSmartParkingBoyTest {
 
   @Test
   void
-      should_throw_not_enough_position_exception_when_parking_given_a_smart_parking_boy_and_a_parking_lot() {
+      should_throw_not_enough_position_exception_when_parking_given_a_super_smart_parking_boy_and_a_parking_lot() {
     // given
     ParkingLot parkingLot = new ParkingLot("1", 0);
     SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
@@ -26,5 +27,23 @@ public class SuperSmartParkingBoyTest {
 
     // then
     assertEquals("Not enough position.", exception.getMessage());
+  }
+
+  @Test
+  void should_get_parking_lot1_when_parking_given_a_super_smart_parking_boy_and_a_parking_boy() {
+    // given
+    ParkingLot parkingLot1 = new ParkingLot("1", 10);
+    ParkingLot parkingLot2 = new ParkingLot("2", 50);
+    SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
+    superSmartParkingBoy.setParkingLots(Arrays.asList(parkingLot1, parkingLot2));
+
+    // when
+    for (int i = 0; i < 8; i++) {
+      superSmartParkingBoy.parking(new Car());
+    }
+
+    // then
+    assertEquals(8, parkingLot1.getEmptyPositionNumber());
+    assertEquals(44, parkingLot2.getEmptyPositionNumber());
   }
 }
